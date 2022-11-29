41f625f332af02a236843a1e1c0eba55687581b3
양원준 <97458548+ywj9811@users.noreply.github.com>
2022-11-29 23:31:02 +0900

    init

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: '만만세',
+  author: '뚱인데요',
+  headerTitle: '뚱이 Blog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
